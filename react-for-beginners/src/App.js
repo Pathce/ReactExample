381c91f92b2ef8088a10ef6e7380774107783a78
Pathce <asfgzx@naver.com>
2022-10-30 18:51:31 +0900

    useEffect 연습 2

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,13 +4,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"Continue"} clickFunc={onClick} />
     </div>
